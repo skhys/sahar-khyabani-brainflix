5329674ac3764b3ecf855f01a4c3ee8a272edec5
Sahar Khyabani <saharkhyabani@Sahars-MacBook-Air.local>
2024-03-29 19:14:09 -0700

    header section styled

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import BfLogo from "./assets/Images/Logo/BrainFlix-logo.svg";
+import "./Header.scss";
+import BfLogo from "../../assets/Images/Logo/BrainFlix-logo.svg";
+import IconImg from "../../assets/Images/Mohan-muruge.jpg";
 
 function Header() {
   return (
@@ -7,6 +9,21 @@ function Header() {
       <header className="header">
         <div className="header__logo">
           <img className="header__img" src={BfLogo} alt="BrainFlix Logo" />
+        </div>
+        <div className="header__wrapper">
+          <div className="header__search">
+            <input
+              className="header__searchbar"
+              type="text"
+              placeholder="Search"
+            />
+          </div>
+          <div className="header__icon">
+            <img className="header__image" src={IconImg} alt="Icon" />
+          </div>
+        </div>
+        <div className="header__upload">
+          <button className="header__uploadbutton">UPLOAD</button>
         </div>
       </header>
     </>
@@ -20,18 +37,18 @@ export default Header;
 //   <img src="BrainFlix-logo.svg" alt="Logo" />
 // </div>
 
-// <div className="search-bar">
-//   <input type="text" placeholder="Search..." />
-//   <button type="submit">
-//     <i className="fa fa-search"></i>
-//   </button>
-// </div>
+/* <div className="search-bar">
+  <input type="text" placeholder="Search..." />
+  <button type="submit">
+    <i className="fa fa-search"></i>
+  </button>
+</div>; */
 
-// <div className="icon">
-//   <img src="Mohan-muruge.jpg" alt="Icon" />
-// </div>
+/* <div className="icon">
+  <img src="Mohan-muruge.jpg" alt="Icon" />
+</div> */
 
-// <div className="upload-button">
-//   <button>Upload</button>
-// </div>
+/* <div className="upload-button">
+  <button>Upload</button>
+</div> */
 // </nav>
